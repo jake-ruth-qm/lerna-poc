5618dd8287077305df13e0bada5b36a98371853c
Jake Ruth <jakeruth@jakes-mbp.myfiosgateway.com>
2021-05-28 12:44:51 -0400

    types

---
+++ packages/test-datasource-1/dist/index.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.sayHello = void 0;
+const sayHello = () => {
+    console.log("hello from datasource 1!");
+};
+exports.sayHello = sayHello;
+//# sourceMappingURL=index.js.map
